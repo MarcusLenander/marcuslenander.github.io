7a97839073bbc731555853e843e8d4809280d5df
MarcusLenander <Marcuslenander96@gmail.com>
2018-11-27 20:50:40 +0100

    started on the bash framework

---
+++ makefile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Dela upp sidan i olika byggstenar. Sedan kan man bygga de olika sidorna.
+
+run:
+  #Bygg hela hemsidan
+  #
+
+math-page.html:
+  #skapar en sida med header
+
+
+clean:
+  rm -f *.html
